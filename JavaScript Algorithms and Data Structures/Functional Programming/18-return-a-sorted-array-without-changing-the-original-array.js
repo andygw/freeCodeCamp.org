f0fcad555561b9f64d4e37211ba2b50562f2adb6
Andrew <48395382+andygw@users.noreply.github.com>
2023-06-10 23:23:10 -0400

    Create 18-return-a-sorted-array-without-changing-the-original-array.js

---
+++ JavaScript Algorithms and Data Structures/Functional Programming/18-return-a-sorted-array-without-changing-the-original-array.js
@@ -0,0 +1,12 @@
+const globalArray = [5, 6, 3, 2, 9];
+
+function nonMutatingSort(arr) {
+  // Only change code below this line
+  const newArray = [].concat(arr);
+  return newArray.sort((a, b) => {
+    return a - b;
+  });
+  // Only change code above this line
+}
+
+nonMutatingSort(globalArray);
