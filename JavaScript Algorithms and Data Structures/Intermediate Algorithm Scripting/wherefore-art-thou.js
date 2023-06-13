970c8b9139329efd8b7b3af9d5cadf7559461b7e
Andrew <48395382+andygw@users.noreply.github.com>
2023-06-12 20:58:25 -0400

    Create wherefore-art-thou.js

---
+++ JavaScript Algorithms and Data Structures/Intermediate Algorithm Scripting/wherefore-art-thou.js
@@ -0,0 +1,21 @@
+function whatIsInAName(collection, source) {
+  // Array to store the final result
+  const new_arr = [];
+
+  // Array of keys used in the source
+  const keys = Object.keys(source)
+  
+  // Loop through each object in the collection
+  for (const item in collection) {
+    // For each of the source keys, check that the collection item
+    // using that key matches the source item using that key. If
+    // it does, add it to the final array
+    if (keys.every(key => collection[item][key] === source[key])) {
+      new_arr.push(collection[item]) 
+    }
+  }
+  
+  return new_arr
+}
+
+whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
